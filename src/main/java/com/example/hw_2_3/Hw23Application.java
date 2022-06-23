bd3ffdcba98d6d742a48b5a60645f31f522c74fe
ElenaR <llinusik@mail.ru>
2022-06-23 16:31:36 +0300

    Initial commit

---
+++ src/main/java/com/example/hw_2_3/Hw23Application.java
@@ -0,0 +1,13 @@
+package com.example.hw_2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw23Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hw_2_3/Hw23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw_2_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
